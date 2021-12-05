6138366d3ddcd72965768595dac2c3f2a5b34941
Andriy <andriy.sachok@gmail.com>
2021-12-05 18:58:05 +0200

    first commit

---
+++ calculator/script.js
@@ -0,0 +1,24 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('calc-button'));
+
+buttons.map((button) => {
+    button.addEventListener('click', (element) => {
+        switch (element.target.innerText) {
+            case 'AC':
+                display.innerText = '';
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = 'Error!';
+                }
+                break;
+            case 'DEL':
+                display.innerText = display.innerText.slice(0, -1);
+                break;
+            default:
+                display.innerText += element.target.innerText;
+        }
+    })
+})
